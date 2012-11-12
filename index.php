f132eff4ba75e737bdaa8042b8dc1b21b45ee30e
Sebastian Sitaru <s.sitaru@gmx.de>
2012-11-12 20:50:50 +0200

    tidy up; eta progress

--- index.php
+++ index.php
@@ -1,16 +1,61 @@
+<?php
+// wie viele durchläufe?
+$mainLoop = 15;
+
+// wie viele durchschnitts-schleifen?
+$avgLoop = 20;
+
+// welcher umfangsmultiplikator?
+$arrayMultiplikator = 300;
+
+
+// ab hier: don't touch this!
+$totalNumbers = array_sum(range(1,$mainLoop))*$arrayMultiplikator*$avgLoop;
+$numbersDone = 0;
+$scriptStartTime = time();
+?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Merge Sort</title>
-    <link rel="stylesheet" href="main.css"/>
-    <script type="text/javascript" src="jquery.js"></script>
+    <title>Merge-Sort</title>
+    <link rel="stylesheet" href="css/main.css"/>
+    <link rel="shortcut icon" href="img/favicon.ico"/>
+    <script type="text/javascript" src="js/jquery.js"></script>
 </head>
 <body>
 <script type="text/javascript">
+    var updateN = 0;
+    var lastProgress = 0;
+    var lastDate = new Date();
+    var nowDate = new Date();
+    var estT = 0;
+    var passedT = 0;
+    
     function updateProgress(p) {
         var w = $("#progressbar-outer").css('width');
         var cw = Math.round(p * (w.slice(0, -2)));
         $("#progressbar-inner").css('width', cw);
+        if((updateN % 2) == 0) { // every 2 updates, update ETA
+            nowDate = new Date();
+
+            var deltaT = nowDate.getTime() - lastDate.getTime();
+            passedT += deltaT;
+            totalT = passedT*(1/p);
+            estT = totalT - passedT;
+            
+            lastProgress = p;
+            lastDate = new Date();
+        }
+        
+        if((p < 1) && (p > 0.3))
+        {
+            $("#progresstext").text(Math.round(p*100)+'% (ETA: '+Math.round(estT/1000)+'s)');
+        } else {
+            $("#progresstext").text(Math.round(p*100)+'%');
+        }
+            
+        
+        updateN++;
     }
 </script>
 <div id="container">
@@ -24,15 +69,12 @@
 <thead>
 <tr>
 <th>Durchlauf-Nr.</th>
-<th>Stellen des Arrays</th>
-<th>Durchschnitts-Zeit bei 10 [ms]</th>
+<th>Umfang des Arrays</th>
+<th>Durchschnitts-Zeit bei <?php echo $avgLoop; ?> [ms]</th>
 </tr>
 </thead>
 <tbody>
 <?php
-$mainLoop = 10;
-$avgLoop = 10;
-$arrayMultiplikator = 300;
 for($i = 1; $i <= $mainLoop; $i++) {
     echo "<tr>";
     echo "<td>".$i."</td>";
@@ -41,13 +83,15 @@ for($i = 1; $i <= $mainLoop; $i++) {
     $ar = array();
     for($z = 0; $z < $avgLoop; $z++) {
         $ar[] = mergeSortWithN($i*$arrayMultiplikator);
-        updateProgress((($i)/$mainLoop), (($z+1)/$avgLoop));
+        $numbersDone += ($i*$arrayMultiplikator);
+        $p = $numbersDone/$totalNumbers;
+        updateProgress($p);
     }
     echo "<td>".(array_sum($ar)/count($ar))."</td>";
     echo "</tr>";
 }
 
-jsOut('$("#progressimg").attr("src", "green_tick.png");');
+jsOut('$("#progressimg").attr("src", "img/green_tick.png");');
 
 function mergeSortWithN($n){
 $zahlen = array();
@@ -105,9 +149,8 @@ function merge($ar1, $ar2) {
    return $return;
 }
 
-function updateProgress($i, $n) {
-    $p = round(($i+($n/100-0.01))*100);
-    jsOut('$("#progresstext").text("'.$p.'%");');
+function updateProgress($i) {
+    $p = round($i*100);
     jsOut('updateProgress('.($p/100).');');
 }
 
@@ -118,7 +161,7 @@ function jsOut($js) {
 ?>
 </tbody>
 </table>
-<div id="footer">Finished on <?php echo date(DateTime::RFC2822); ?> </div>
+<div id="footer">Finished on <?php echo date(DateTime::RFC2822); ?> [&#916;t = <?php echo time()-$scriptStartTime; ?>s] </div>
 </div>
 </body>
 </html>
